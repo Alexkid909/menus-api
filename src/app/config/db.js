455335e821d99cc6566012bacee328dcb5b20202
Alex <alex@cvhemistrymarketing.co.uk>
2018-06-09 12:26:54 +0100

    Adding server.

--- src/app/config/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url : "mongodb://alex:A1exkid909@ds247270.mlab.com:47270/menus"
-};
---
+++ src/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : "mongodb://admin:A!exkid909@ds247270.mlab.com:47270/menu"
+};
