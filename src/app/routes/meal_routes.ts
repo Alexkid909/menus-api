146ccdf6a9b560b4e67f9ad9aa560261610f1967
Alex <alex.robinson578@gmail.com>
2019-01-06 10:28:49 +0000

    ...refactors request handlers for GET, POST, PUT and DELETE for single Meals in to MealsService. ...refactors Meal routes to use MealsService request handlers.

--- src/app/routes/meal_routes.ts
+++ src/app/routes/meal_routes.ts
@@ -1,70 +1,27 @@
 import {Application, NextFunction, Request, Response} from "express";
-import {MongoError} from "mongodb";
-import {ObjectID} from "bson";
-import {Meal} from "../classes/meal";
 import {MealService} from "../services/meals.service";
 
-
 module.exports = (app: Application, db: any) => {
-    const standardErrorMessage = () => {
-        return {error: 'An error has occured'}
-    };
 
     const mealsService = new MealService(db);
-
-    const mealsCollection = db.collection('meals');
-    const mealFoodsCollection = db.collection('mealFoods');
 
     app.get('/meals', (req: Request, res: Response, next: NextFunction) => {
         mealsService.getMeals(req, res, next);
     });
 
-    app.post('/meals', (req: Request, res: Response) => {
-        const meal = new Meal(req.body.name);
-        console.log('new meal', meal);
-        if(!meal.name) {
-            res.send({message:'Please provide a name for this meal'});
-        } else {
-            mealsCollection.insert(meal, (err: any, result: any) => {
-                (err) ? res.send(standardErrorMessage()) : res.send(result.ops[0]);
-            });
-        }
+    app.post('/meals', (req: Request, res: Response, next: NextFunction) => {
+        mealsService.createMeal(req, res, next);
     });
 
-    app.get('/meals/:id', (req: Request, res: Response) => {
-        const details = {'_id' : new ObjectID(req.params.id)};
-
-        mealsCollection.findOne(details, (err: any, result: any) => {
-            (err) ? res.send(standardErrorMessage()) : res.send(result);
-        });
+    app.get('/meals/:id', (req: Request, res: Response, next: NextFunction) => {
+        mealsService.getMeal(req, res, next);
     });
 
-    app.put('/meals/:id', (req: Request, res: Response) => {
-        const meal = new Meal(req.body.name, req.body.foods);
-        (!meal.name) && res.send('Please provide a name for this meal');
-        console.log('updated meal', meal);
-        const id = req.params.id;
-        const details = {'_id': new ObjectID(id)};
-        mealsCollection.updateOne(details, {$set: meal}).then((success: any) => {
-            mealsCollection.findOne(details, (err: MongoError, item: any) => {
-                if (err) {
-                    res.send({'error':'An error has occurred'});
-                } else {
-                    console.log('item', item);
-                    res.send(item);
-                }
-            });
-        }, (error: any) => {
-            console.log('error', error)
-        })
+    app.put('/meals/:id', (req: Request, res: Response, next: NextFunction) => {
+        mealsService.updateMeal(req, res, next);
     });
 
-    app.delete('/meals/:id', (req: Request, res: Response) => {
-        const id = new ObjectID(req.params.id);
-        const details = {'_id' : id};
-        mealsCollection.remove(details, (err: any, result: any) => {
-            const success = {message: `Meal ${id} deleted!`};
-            (err) ? res.send(standardErrorMessage()) : res.send(JSON.stringify(success));
-        });
+    app.delete('/meals/:id', (req: Request, res: Response, next: NextFunction) => {
+        mealsService.deleteMeal(req, res, next);
     });
 };
--- src/app/services/meals.service.ts
+++ src/app/services/meals.service.ts
@@ -1,8 +1,5 @@
-import {MealFoodLink} from "../classes/mealFoodLink";
 import {ObjectID} from "bson";
-import {Food} from "../classes/food";
-import {Collection} from "mongodb";
-import {MealFood} from "../classes/mealFood";
+import {Collection, MongoError} from "mongodb";
 import {NextFunction, Request, Response} from "express";
 import {Meal} from "../classes/meal";
 import {ApiErrorBody} from "../classes/apiErrorBody";
@@ -17,23 +14,54 @@ export class MealService {
 
     getMeals(req: Request, res: Response, next: NextFunction) {
         this.mealsCollection.find({}).toArray((err: any, result: any) => {
-            (err) ? res.status(500).send(new ApiErrorBody()) : res.send(result);
+            (err) ? res.send(new ApiErrorBody()) : res.send(result);
         });
     }
 
     getMeal(req: Request, res: Response, next: NextFunction) {
-
+        const details = {'_id' : new ObjectID(req.params.id)};
+        this.mealsCollection.findOne(details, (err: any, result: any) => {
+            (err) ? res.send(new ApiErrorBody()) : res.send(result);
+        });
     }
 
     createMeal(req: Request, res: Response, next: NextFunction) {
-
+        const meal = new Meal(req.body.name);
+        if(!meal.name) {
+            res.send({message:'Please provide a name for this meal'});
+        } else {
+            this.mealsCollection.insert(meal, (err: any, result: any) => {
+                (err) ? res.status(500).send(new ApiErrorBody()) : res.send(result.ops[0]);
+            });
+        }
     }
 
     deleteMeal(req: Request, res: Response, next: NextFunction) {
-
+        const id = new ObjectID(req.params.id);
+        const details = {'_id' : id};
+        this.mealsCollection.remove(details, (err: any, result: any) => {
+            const success = {message: `Meal ${id} deleted!`};
+            (err) ? res.send(new ApiErrorBody()) : res.send(JSON.stringify(success));
+        });
     }
 
     updateMeal(req: Request, res: Response, next: NextFunction) {
-
+        const meal = new Meal(req.body.name, req.body.foods);
+        (!meal.name) && res.send('Please provide a name for this meal');
+        console.log('updated meal', meal);
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        this.mealsCollection.updateOne(details, {$set: meal}).then((success: any) => {
+            this.mealsCollection.findOne(details, (err: MongoError, item: any) => {
+                if (err) {
+                    res.send({'error':'An error has occurred'});
+                } else {
+                    console.log('item', item);
+                    res.send(item);
+                }
+            });
+        }, (error: any) => {
+            console.log('error', error)
+        })
     }
 }
